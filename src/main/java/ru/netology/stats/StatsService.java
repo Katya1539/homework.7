42a472fe83587111dbf2f4929786745d97e1f5bb
Katya1539 <erkenovakatya@mail.ru>
2024-04-20 10:48:02 +0300

    MiddleSales

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -8,4 +8,14 @@ public class StatsService {
         }
         return all;
     }
+
+    public int sumMiddle(int[] sales) {
+        int all = 0;
+        for (int i =0; i < sales.length; i++) {
+            all = all + sales[i];
+        }
+        int middle = all / 12;
+        return middle;
+
+    }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -16,4 +16,16 @@ public class StatsServiceTest {
 
         Assertions.assertEquals(expectedSales, actualSales);
     }
+
+    @Test
+    public void middleSum() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMiddle = 15;
+        int actualMiddle = service.sumMiddle(sales);
+
+        Assertions.assertEquals(expectedMiddle, actualMiddle);
+    }
 }
